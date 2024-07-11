dd97603af169523ca676bbab9225718372f6743d
Ajinkya Chirde <ajinkyachirde07@gmail.com>
2024-07-11 19:05:41 +0530

    Add files via upload

---
+++ Snapshop E-commerce Web/Snapshop-main/src/components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import LinkFooter from "./components/LinkFooter";
+import PrivacyFooter from "./components/PrivacyFooter";
+import GoToTopBtn from "./components/GoToTopBtn";
+
+export default function Footer() {
+  return (
+    <>
+      <GoToTopBtn />
+      <div className="py-6 w-full md:flex md:justify-around md:items-center">
+        <LinkFooter />
+        <PrivacyFooter />
+      </div>
+      <p className="text-center">© 2024 Snap Shop All Rights Reserved </p>
+      <p className="text-center py-3 mt-4 text-[15px] w-full bg-black text-white">
+        Made by{" "}
+        <a className="underline" href="https://github.com/Ajinkyachirde07">
+          {" "}
+          Ajinkya Chirde{" "}
+        </a>{" "}
+        with ❤️
+      </p>
+    </>
+  );
+}
